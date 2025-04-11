10382d83506c6dcc6f3e23107c13ab2290054eb2
Alexander Ng <alexng353@gmail.com>
2025-04-11 01:30:52 -0700

    Initial Commit

---
+++ scaleTests/scaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  scaleTests.swift
+//  scaleTests
+//
+//  Created by Alexander Ng on 2025-04-11.
+//
+
+import Testing
+@testable import scale
+
+struct scaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
